0dcaeb8940a25c64d8c0e26a127982e086c0e940
Himanshu Minocha <manshu4@gmail.com>
2019-09-06 23:29:59 -0400

    People table now contains UUID for the device

--- SQL/People.sql
+++ SQL/People.sql
@@ -1,5 +1,6 @@
 Create Table People(
 	id INT not Null primary key AUTO_INCREMENT,
+    UUID VARCHAR(255), 
     currentLatitude DOUBLE, 
     currentLongitude DOUBLE, 
     usualSchool VARCHAR(255)
