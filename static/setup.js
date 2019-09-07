b34066e086d441bafe05546090c9a9a8aab62496
jmoh3 <jmoh3@illinois.edu>
2019-09-07 14:28:13 -0400

    Update zoom location

--- static/setup.js
+++ static/setup.js
@@ -5,15 +5,13 @@ window.onload = function() {
     var schoolInput = document.getElementById("schoolInput");
     var goInput = document.getElementById("schoolGo");
 
-    var mapOverlay = document.getElementById("overlay")
-
     console.log(schoolInput)
 
     // Execute a function when the user releases a key on the keyboard
     schoolInput.addEventListener("keyup", function(event) {
         // Number 13 is the "Enter" key on the keyboard
         if (event.keyCode === 13) {
-            event.preventDefault();    
+            event.preventDefault();
             goInput.click();
         }
     });
@@ -37,14 +35,14 @@ function fadeOutEffect() {
             clearInterval(fadeEffect);
         }
     }, 100);
-}
+};
 
 function showModal() {
     setTimeout(function() {
         $("#exampleModal").modal()
     }, 2000);
-}
+};
 
 function enableMap() {
     document.getElementById("overlay").style.display = "none";
-}
+};
